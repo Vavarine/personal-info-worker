b450b01f313b6151a689eaf4cd503b229f1af538
Vavarine <evailson.m.barbosa@gmail.com>
2025-03-24 21:33:07 -0300

    code editor

---
+++ src/json-data.ts
@@ -0,0 +1,72 @@
+export const jsonData = {
+  contact: {
+    email: "contato@evailson.dev",
+    discord: "https://discord.com/users/394131988023476235",
+  },
+  personalInfo: {
+    name: "Evailson Barbosa",
+    birthdate: "2001-09-12",
+  },
+  links: {
+    linkedin: "https://www.linkedin.com/in/evailson/",
+    github: "https://github.com/vavarine",
+    portfolio: "https://github.com/vavarine",
+  },
+  location: {
+    country: "Brazil",
+    state: "São Paulo",
+    city: "São Paulo",
+  },
+  languages: [
+    "English",
+    "Portuguese",
+  ],
+  workExperience: [
+    {
+      company: {
+        name: "e-Plus",
+        url: "https://www.agenciaeplus.com.br/",
+      },
+      role: "Desenvolvedor Full Stack Web Pleno",
+      description: "Desenvolvedor Full Stack Web Pleno",
+      period: {
+        start: "2021-01-01",
+        end: null,
+      },
+    }
+  ],
+  academicBackground: [
+    {
+      degree: "Pós-graduação em Arquitetura de Software",
+      institution: "FIAP",
+      period: {
+        start: "2025-03-01",
+        end: null,
+      },
+    },
+    {
+      degree: "Análise e Desenvolvimento de Sistemas",
+      institution: "Fatec São Caetano do Sul",
+      period: {
+        start: "2019-01-01",
+        end: "2022-01-01",
+      },
+    },
+    {
+      degree: "Técnico em Análise e Desenvolvimento de Sistemas",
+      institution: "Etec Mauá",
+      period: {
+        start: "2018-01-01",
+        end: "2019-06-01",
+      },
+    },
+    {
+      degree: "Técnico em Administração",
+      institution: "Etec Mauá",
+      period: {
+        start: "2017-01-01",
+        end: "2018-06-01",
+      },
+    }
+  ]
+}
